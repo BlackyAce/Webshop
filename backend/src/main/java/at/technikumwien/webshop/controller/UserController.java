f864dc001aa72609098c8161ad5c5a59e4c4d38d
BlackyAce <aleksandra.edlmayer@gmail.com>
2023-09-27 10:11:23 +0200

    Delete User New

--- backend/src/main/java/at/technikumwien/webshop/controller/UserController.java
+++ backend/src/main/java/at/technikumwien/webshop/controller/UserController.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +42,13 @@ public class UserController {
         return ResponseEntity.created(URI.create("http://localhost:8080/users")).body(createdUser);
     }
 
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+    
     private User fromDTO(UserDTO userDTO) {
         User user = new User();
         user.setUsername(userDTO.getUsername());
