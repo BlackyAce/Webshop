aa9b311f304e2a2f7bd10f1976edbf56edc9ce79
s.schirmboeck <sandrina.schirmboeck@gmx.at>
2023-09-29 13:54:15 +0200

    bug fix create product

--- backend/src/main/java/at/technikumwien/webshop/model/Product.java
+++ backend/src/main/java/at/technikumwien/webshop/model/Product.java
@@ -3,6 +3,7 @@ package at.technikumwien.webshop.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 /**
@@ -12,7 +13,7 @@ import jakarta.persistence.Id;
 public class Product {
     
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
